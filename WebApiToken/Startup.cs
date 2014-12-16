4d5917f1535b0c786d6b0221cb6cc7950169cc58
diedona <diedona@gmail.com>
2014-12-16 14:09:26 -0200

    start do projeto

---
+++ WebApiToken/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApiToken.Startup))]
+namespace WebApiToken
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder App)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            App.UseWebApi(config);
+        }
+    }
+}
